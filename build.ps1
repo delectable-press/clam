5b2a604c7ccedb18fcb80d2b6beeef8fde61d6bd
Rob Landers <landers.robert@gmail.com>
2016-01-18 22:28:53 -0500

    Use outside docker inside container : #10

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 docker build -t clamp/builder images
 $path = "/" + $HOME.Replace("\", "/").Replace(":", "").Replace("C/", "c/")
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $path/.docker/config.json:/root/.docker/config.json --rm clamp/builder --runs --plugins --seq --libs --services
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $path/.docker/config.json:/root/.docker/config.json -v /usr/local/bin/docker:/usr/bin/docker --rm clamp/builder --runs --plugins --seq --libs --services
