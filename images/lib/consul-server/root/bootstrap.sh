762fc8fc71b338858ecfe253a550fd8c0f54beff
Rob Landers <landers.robert@gmail.com>
2016-02-02 01:23:02 -0500

    Much more stable consul

--- images/lib/consul-server/root/bootstrap.sh
+++ images/lib/consul-server/root/bootstrap.sh
@@ -5,34 +5,25 @@ source /common.sh
 self=$(curl -s -H 'Accept: application/json' ${META_URL}/self/container| jq -r .name)
 
 echo "Starting bootstrap server"
-consul agent $(DC) -config-dir /etc/consul.d/bootstrap -bind $(get_container_primary_ip $self) &
+consul agent $(DC) -config-dir /etc/consul.d/bootstrap -advertise $(get_container_primary_ip $self) &
 SERVER="$!"
 sleep 5
 
 echo "Waiting for cluster to come online"
 
-online=$(consul members | grep alive | grep server | wc -l)
-while [ $online -lt 3 ]
+online=1
+while [ $online -lt 4 ]
 do
-    echo "Only $online servers, waiting for at least 3"
+    online=$(consul members | grep alive | grep server | wc -l)
+    echo "Only $online servers, waiting for at least 4"
     sleep 5
 done
 
 echo "Enough nodes are online, preparing to rejoin as client"
+sleep 30
 consul leave
 wait $SERVER
 
-consul agent $(DC) -config-dir /etc/consul.d/client -bind $(get_container_primary_ip $self) &
+consul agent $(DC) -config-dir /etc/consul.d/client -advertise $(get_container_primary_ip $self) -retry-join consul &
 CLIENT="$!"
-
-echo "Client started - entering self-heal mode"
-online=$(consul members | grep alive | grep server | wc -l)
-while [ $online -gt 2 ]
-do
-    sleep 30
-done
-
-echo "Lost enough nodes to require self-healing"
-echo "Dieing in order to recreate cluster state..."
-consul leave
 wait $CLIENT
