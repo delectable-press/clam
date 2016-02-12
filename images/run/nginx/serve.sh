699fe953a47db3682ce071df7bb8fb04cb5b50d3
Rob Landers <landers.robert@gmail.com>
2016-02-12 07:32:20 -0500

    Added swarm building script for windows, updated yml file to version 2

--- images/run/nginx/serve.sh
+++ images/run/nginx/serve.sh
@@ -2,10 +2,4 @@
 
 printenv
 
-cat > /etc/nginx/upstreams/proxy.conf <<EOF
-upstream php {
-	server ${PHP_PROXY_1_PORT_9000_TCP_ADDR}:9000;
-}
-EOF
-
 exec /usr/sbin/nginx
---
+++ stacks/destroy-swarm.ps1
@@ -0,0 +1,5 @@
+docker-machine rm -f manager
+sleep 1
+docker-machine rm -f agent1
+sleep 1
+docker-machine rm -f generator
