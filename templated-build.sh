84e1fa4b768350562b9e2130563f54f02d24a2b2
Rob Landers <landers.robert@gmail.com>
2016-02-18 12:00:04 -0500

    Use templated build

---
+++ templated-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apply_shell_expansion() {
+    declare file="$1"
+    declare data=$(< "$file")
+    declare delimiter="__apply_shell_expansion_delimiter__"
+    declare command="cat <<$delimiter"$'\n'"$data"$'\n'"$delimiter"
+    eval "$command"
+}
+
+BUILD_NUMBER=$3
+BUILD_TYPE=$1
+BUILD_NAME=$2
+BUILD=images/$BUILD_TYPE/$BUILD_NAME
+IMAGE=clamp/$BUILD_TYPE-$BUILD_NAME
+$(cat $BUILD/Dockerfile | grep ENV | cut -d \  -f 2 | cut -d = -f 1 | xargs -L1 ./export.sh)
+apply_shell_expansion $BUILD/Dockerfile > $BUILD/Dockerfile
+docker build -t $IMAGE:$$BUILD_NUMBER $BUILD
