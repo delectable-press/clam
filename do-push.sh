25271365255981d472297f57735adb0029644b7e
Rob Landers <landers.robert@gmail.com>
2016-02-18 14:24:06 -0500

    Exit if a push dies

--- do-push.sh
+++ do-push.sh
@@ -2,15 +2,45 @@
 
 build=$1
 tag=$2
+pids=""
+WAITALL_DELAY=10
 
 push () {
   image=$1
   docker push $image:$build &
+  pids="$pids $!"
 
   if [ "$tag" != "" ]
   then
     docker push $image:$tag &
+    pids="$pids $!"
   fi
+}
+
+debug() { echo "DEBUG: $*" >&2; }
+
+waitall() { # PID...
+  ## Wait for children to exit and indicate whether all exited with 0 status.
+  local errors=0
+  while :; do
+    debug "Processes remaining: $*"
+    for pid in "$@"; do
+      shift
+      if kill -0 "$pid" 2>/dev/null; then
+        debug "$pid is still alive."
+        set -- "$@" "$pid"
+      elif wait "$pid"; then
+        debug "$pid exited with zero exit status."
+      else
+        debug "$pid exited with non-zero exit status."
+        ((++errors))
+      fi
+    done
+    (("$#" > 0)) || break
+    # TODO: how to interrupt this sleep when a child terminates?
+    sleep ${WAITALL_DELAY:-1}
+   done
+  ((errors == 0))
 }
 
 push clamp/lib-base
@@ -26,4 +56,4 @@ push clamp/run-php-fpm
 push clamp/run-plugin
 push clamp/run-wordpress
 push clamp/srv-composer
-wait
+waitall $pids
